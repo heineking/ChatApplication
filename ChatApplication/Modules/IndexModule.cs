b751db5bc8543c2688111495375730b5569383e3
emil.heineking <ebheineking@gmail.com>
2017-02-26 10:30:38 -0500

    working razor view page being rendered

--- ChatApplication/Modules/IndexModule.cs
+++ ChatApplication/Modules/IndexModule.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Nancy;
+﻿using Nancy;
 
 namespace ChatApplication.Modules
 {
@@ -10,7 +6,7 @@ namespace ChatApplication.Modules
     {
         public IndexModule()
         {
-            Get["/"] = parameters => "Hello World";
+            Get["/"] = parameters => View["index.cshtml"];
         }
     }
 }
